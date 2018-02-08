f95e2417b207a2ed9a670ceed3cc39c03ced638e
Omar Madrid <omar.madrid@thermofisher.com>
2018-02-08 11:25:11 -0800

    Init commit

---
+++ src/main/java/com/madro/zippy/ZippylogicApplication.java
@@ -0,0 +1,12 @@
+package com.madro.zippy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZippylogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZippylogicApplication.class, args);
+	}
+}
